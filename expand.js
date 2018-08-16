1d1e23819687371ec4676410237f5e8a1f148012
Elizabeth Lawley <mamamusings@gmail.com>
2018-08-16 12:47:31 -0400

    Beta version of design complete

--- expand.js
+++ expand.js
@@ -1,4 +1,40 @@
-var headings = document.querySelectorAll("main h2");
+jQuery(document).ready(function($) {
+    var setClass = function() {
+      var ww = document.body.clientWidth;
+      if (ww >= 801) {
+        console.log("onload more than 800");
+        $('svg').removeClass('fa-caret-left');
+        $('svg').addClass('fa-caret-down');
+      };
+    };
+
+    var alterClass = function() {
+        var ww = document.body.clientWidth;
+        if (ww >= 801) {
+          console.log("resize above 800");
+          $('svg').removeClass('fa-caret-left');
+          $('svg').removeClass('fa-caret-down');
+          $('svg').addClass('fa-caret-down');
+          $('.panel').show();
+        }
+        else if (ww <= 800) {
+            console.log("resize under 800");
+            $('svg').removeClass('fa-caret-down');
+            $('svg').removeClass('fa-caret-left');
+           $('svg').addClass('fa-caret-left');  
+           $('.panel').hide();  
+        };
+      }
+
+    $(window).resize(function(){
+      alterClass();
+    });
+
+    //Fire initial setClass when the page first loads so icons match media query:
+    setClass();
+  });
+  
+var headings = document.querySelectorAll(".subhead");
 var i;
 
 for (i = 0; i < headings.length; i++) {
@@ -6,5 +42,6 @@ for (i = 0; i < headings.length; i++) {
         /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         $(panel).toggle();
+        $(this).children('svg').toggleClass("fa-caret-down fa-caret-left");
     });
 } 
